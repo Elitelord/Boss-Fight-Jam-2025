da4cfe1ad8e6b516f12903f6131e0859ee2f4f2a
Rishi Nigam <rishin@Mac.attlocal.net>
2025-07-21 15:01:08 -0500

    startedanimations

--- Assets/move.cs
+++ Assets/move.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using TMPro; // TextMeshPro support
 
 public class move : MonoBehaviour
 {
@@ -9,12 +10,22 @@ public class move : MonoBehaviour
     private bool isGrounded;
 
     [Header("Shooting")]
-    public GameObject bulletPrefab; // Assign your bullet prefab in Inspector
+    public GameObject bulletPrefab;
     public float bulletSpeed = 10f;
+
+    [Header("Animator")]
+    private Animator animator;
+    private bool jumpTriggered = false;
+
+    [Header("Debug UI (TMP)")]
+    public TMP_Text speedText;
+    public TMP_Text isJumpingText;
+    public TMP_Text jumpText;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     void Update()
@@ -22,12 +33,27 @@ public class move : MonoBehaviour
         HandleMovement();
         HandleJump();
         HandleShooting();
+
+        // Animator sync
+        animator.SetBool("IsJumping", !isGrounded);
+
+        // Debug UI display
+        speedText.text = "Speed: " + animator.GetFloat("Speed").ToString("F2");
+        isJumpingText.text = "IsJumping: " + animator.GetBool("IsJumping");
+        jumpText.text = "Jump Triggered: " + jumpTriggered;
     }
 
     void HandleMovement()
     {
         float moveInput = Input.GetAxisRaw("Horizontal");
         rb.linearVelocity = new Vector2(moveInput * moveSpeed, rb.linearVelocity.y);
+
+        // Flip the player
+        if (moveInput != 0)
+            transform.localScale = new Vector3(Mathf.Sign(moveInput), 1, 1);
+
+        // Pass signed speed to animator
+        animator.SetFloat("Speed", moveInput);
     }
 
     void HandleJump()
@@ -35,8 +61,16 @@ public class move : MonoBehaviour
         if (isGrounded && Input.GetKeyDown(KeyCode.Space))
         {
             rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            animator.SetTrigger("Jump");
+            jumpTriggered = true;
+            Invoke(nameof(ResetJumpTriggerDisplay), 0.2f);
             isGrounded = false;
         }
+    }
+
+    void ResetJumpTriggerDisplay()
+    {
+        jumpTriggered = false;
     }
 
     void HandleShooting()
