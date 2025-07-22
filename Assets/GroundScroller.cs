73ba041401efcaf1aab9a5c165209297e387ee08
Rishi Nigam <rishin@Mac.attlocal.net>
2025-07-22 12:01:07 -0500

    madesmallgame

--- Assets/GroundScroller.cs
+++ Assets/GroundScroller.cs
@@ -2,20 +2,17 @@ using UnityEngine;
 
 public class GroundScroller : MonoBehaviour
 {
+    public GameObject[] groundPieces; // Assign all ground pieces in the inspector
     public float scrollSpeed = 5f;
-    public float resetPositionX = -20f; // X position at which the ground resets
-    public float startPositionX = 20f;  // X position it resets to
 
     void Update()
     {
-        // Move the ground left
-        transform.Translate(Vector3.left * scrollSpeed * Time.deltaTime);
-
-        // If ground moves past reset point, move it back to start
-        if (transform.position.x <= resetPositionX)
+        foreach (GameObject ground in groundPieces)
         {
-            Vector3 newPos = new Vector3(startPositionX, transform.position.y, transform.position.z);
-            transform.position = newPos;
+            if (ground != null)
+            {
+                ground.transform.Translate(Vector3.left * scrollSpeed * Time.deltaTime);
+            }
         }
     }
 }
