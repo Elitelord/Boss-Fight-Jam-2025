ff009cce8101d0074b5a9193fbdc988f7ac06780
Elitelord <sameeragarwal007@outlook.com>
2025-07-23 17:46:59 -0500

    hopefully fixing bullets idk

--- Assets/WeaponScript.cs
+++ Assets/WeaponScript.cs
@@ -9,33 +9,45 @@ public class Weapon : MonoBehaviour
     public Transform muzzlePoint;
 
     
-    public void Shoot()
+   // In Weapon.cs
+public void Shoot()
+{
+    Debug.Log("Shoot() method called on " + gameObject.name);
+    if (weaponData.bulletPrefab != null && muzzlePoint != null)
     {
-       
-        Debug.Log("Shoot() method called on " + gameObject.name);
-        if (weaponData.bulletPrefab != null && muzzlePoint != null)
-        {
-            Debug.Log("Bullet Shot");
-            Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            Vector2 direction = (mouseWorldPos - muzzlePoint.position).normalized;
-            
-            GameObject bullet = Instantiate(weaponData.bulletPrefab, muzzlePoint.position, muzzlePoint.rotation);
-            Rigidbody2D bulletRb = bullet.GetComponent<Rigidbody2D>();
-            bulletRb.linearVelocity = direction * 10f;
-            // bulletRb.linearVelocity = direction * bulletSpeed;
-        }
+        Debug.Log("Bullet Shot");
+        Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 direction = (mouseWorldPos - muzzlePoint.position).normalized;
+        
+        // Instantiate the bullet
+        GameObject bulletObject = Instantiate(weaponData.bulletPrefab, muzzlePoint.position, Quaternion.identity);
 
-       
-        if (weaponData.fireSound != null)
-        {
-            
-            AudioSource.PlayClipAtPoint(weaponData.fireSound, transform.position);
-        }
+        
+        // Rotate the bullet to face the direction it's moving
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        bulletObject.transform.rotation = Quaternion.Euler(0, 0, angle);
 
-      
-        if (weaponData.muzzleFlashEffect != null && muzzlePoint != null)
+        // Get the Bullet script and set its damage
+        Bullet bulletScript = bulletObject.GetComponent<Bullet>();
+        if (bulletScript != null)
         {
-            Instantiate(weaponData.muzzleFlashEffect, muzzlePoint.position, muzzlePoint.rotation);
+            bulletScript.damage = weaponData.damage;
         }
+        
+
+        // Set the bullet's velocity
+        Rigidbody2D bulletRb = bulletObject.GetComponent<Rigidbody2D>();
+        bulletRb.linearVelocity = direction * 10f; // You can use a variable for bullet speed here
     }
+
+    if (weaponData.fireSound != null)
+    {
+        AudioSource.PlayClipAtPoint(weaponData.fireSound, transform.position);
+    }
+
+    if (weaponData.muzzleFlashEffect != null && muzzlePoint != null)
+    {
+        Instantiate(weaponData.muzzleFlashEffect, muzzlePoint.position, muzzlePoint.rotation);
+    }
+}
 }
